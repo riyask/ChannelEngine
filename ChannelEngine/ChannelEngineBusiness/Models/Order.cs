267e707a4c9c144b244fd4dc50b6e14421d436e0
riyask <riyas.k13@gmail.com>
2022-01-22 22:56:31 +0100

    Project setup and initial commit

---
+++ ChannelEngine/ChannelEngineBusiness/Models/Order.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChannelEngineBusiness.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class BillingAddress
+    {
+        public string Line1 { get; set; }
+        public object Line2 { get; set; }
+        public object Line3 { get; set; }
+        public string Gender { get; set; }
+        public object CompanyName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public string HouseNr { get; set; }
+        public object HouseNrAddition { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public object Region { get; set; }
+        public string CountryIso { get; set; }
+        public object Original { get; set; }
+    }
+
+    public class ShippingAddress
+    {
+        public string Line1 { get; set; }
+        public object Line2 { get; set; }
+        public object Line3 { get; set; }
+        public string Gender { get; set; }
+        public object CompanyName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public string HouseNr { get; set; }
+        public object HouseNrAddition { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public object Region { get; set; }
+        public string CountryIso { get; set; }
+        public object Original { get; set; }
+    }
+
+    public class Line
+    {
+        public string Status { get; set; }
+        public bool IsFulfillmentByMarketplace { get; set; }
+        public string Gtin { get; set; }
+        public string Description { get; set; }
+        public object StockLocation { get; set; }
+        public double UnitVat { get; set; }
+        public double LineTotalInclVat { get; set; }
+        public double LineVat { get; set; }
+        public double OriginalUnitPriceInclVat { get; set; }
+        public double OriginalUnitVat { get; set; }
+        public double OriginalLineTotalInclVat { get; set; }
+        public double OriginalLineVat { get; set; }
+        public double OriginalFeeFixed { get; set; }
+        public object BundleProductMerchantProductNo { get; set; }
+        public object JurisCode { get; set; }
+        public object JurisName { get; set; }
+        public double VatRate { get; set; }
+        public List<object> ExtraData { get; set; }
+        public string ChannelProductNo { get; set; }
+        public string MerchantProductNo { get; set; }
+        public int Quantity { get; set; }
+        public int CancellationRequestedQuantity { get; set; }
+        public double UnitPriceInclVat { get; set; }
+        public double FeeFixed { get; set; }
+        public double FeeRate { get; set; }
+        public string Condition { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+    }
+
+
+    public class Content
+    {
+        public int Id { get; set; }
+        public string ChannelName { get; set; }
+        public int ChannelId { get; set; }
+        public string GlobalChannelName { get; set; }
+        public int GlobalChannelId { get; set; }
+        public string ChannelOrderSupport { get; set; }
+        public string ChannelOrderNo { get; set; }
+        public string MerchantOrderNo { get; set; }
+        public string Status { get; set; }
+        public bool IsBusinessOrder { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public object MerchantComment { get; set; }
+        public BillingAddress BillingAddress { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
+        public double SubTotalInclVat { get; set; }
+        public double SubTotalVat { get; set; }
+        public double ShippingCostsVat { get; set; }
+        public double TotalInclVat { get; set; }
+        public double TotalVat { get; set; }
+        public double OriginalSubTotalInclVat { get; set; }
+        public double OriginalSubTotalVat { get; set; }
+        public double OriginalShippingCostsInclVat { get; set; }
+        public double OriginalShippingCostsVat { get; set; }
+        public double OriginalTotalInclVat { get; set; }
+        public double OriginalTotalVat { get; set; }
+        public List<Line> Lines { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public object CompanyRegistrationNo { get; set; }
+        public object VatNo { get; set; }
+        public string PaymentMethod { get; set; }
+        public object PaymentReferenceNo { get; set; }
+        public double ShippingCostsInclVat { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime OrderDate { get; set; }
+        public object ChannelCustomerNo { get; set; }
+    }
+
+    public class ValidationErrors
+    {
+    }
+
+    public class Order
+    {
+        public List<Content> Content { get; set; }
+        public int Count { get; set; }
+        public int TotalCount { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int StatusCode { get; set; }
+        public object LogId { get; set; }
+        public bool Success { get; set; }
+        public object Message { get; set; }
+        public ValidationErrors ValidationErrors { get; set; }
+    }
+}
---
+++ ChannelEngine/ChannelEngineConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ChannelEngineBusiness;
+namespace ChannelEngineConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderManagement om = new OrderManagement();
+            var result= om.GetAllOrders();
+            //var s= GetAllOrders();
+            //GetAsync();
+            Console.WriteLine("Hello World!");
+        }
+
+        public static object GetAllOrders()
+        {
+            var result = Task.Run(async () => await GetAsync()).Result;
+            return result;
+        }
+        public static async Task<HttpResponseMessage> GetAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(BearerTokenName, token);
+                var result = await client.GetAsync("https://api-dev.channelengine.net/api/v2/orders?statuses=IN_PROGRESS&apikey=541b989ef78ccb1bad630ea5b85c6ebff9ca3322").ConfigureAwait(false);
+
+                if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return result;
+                }
+                else
+                {
+                    return null;
+                   // throw new HttpResponseException(new HttpResponseMessage(result.StatusCode) { Content = new StringContent(result.ReasonPhrase) });
+                }
+            }
+        }
+    }
+}
